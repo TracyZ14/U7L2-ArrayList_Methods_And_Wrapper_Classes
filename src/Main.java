2d90c2cea7a0816cfafcb359da7ff4b00061f75c
Tracy Zhou <TracyZ14@nycstudents.net>
2024-02-01 14:04:25 -0500

    Added the Car, UsedCarLot, and CarRunner classes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.ArrayList;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        ArrayList<Integer> numList = new ArrayList<Integer>();
+        numList.add(3);
+        numList.add(0, 6);
+        numList.add(7);
+        numList.add(2, 8);
+        numList.set(3, 9);
+        numList.add(1, 2);
+        numList.add(1, numList.size());
+        numList.remove(5);
+        numList.set(numList.size() - 1, 0);
+        System.out.println(numList);
     }
 }
